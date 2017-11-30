9e1bdfcc766c9890662c1e068ad90facd9c2b82c
ciancostelloe <cian.costelloe@yahoo.ie>
2017-11-30 16:20:02 +0000

    Testing sequencce

--- initPODs.py
+++ initPODs.py
@@ -5,14 +5,21 @@
 ##
 ##Initialise PODs
 
+##ADI BLUE = 1E4056
+##ADI ORANGE = FF7200
+##ADI GREEN = 27B34F
+##ADI PURPLE = 7C4A8B
+##ADI RED = 990033
+##ADI BLUE 2 = 009FBD
+##ADI BLUE 3 = 3DDCE6
+
 #############################################################
 
 ##Imports and Definitions
 import serial
 import time
-#from Tkinter import *
 
-print('Initialising connection');
+print('Initialising connection\n');
 
 arduinoSerialData = serial.Serial(
   
@@ -25,10 +32,10 @@ arduinoSerialData = serial.Serial(
    )
    
 time.sleep(6)
-print('Port opened successfully')
+print('Port opened successfully\n')
 
 ######################Read Switch Values######################
-print('Running switch diagnostics')
+print('Running switch diagnostics:')
 
 for i in range (0,4):
     arduinoSerialData.write('READ_SW\r'.encode())
@@ -36,16 +43,49 @@ for i in range (0,4):
     time.sleep(0.1)
 
     if len(switchState) > 4:
-        print(switchState);
+        #print(switchState);
         array = list(str(switchState))
-        print(array)
+        #print(array)
         for x, val in enumerate(array):
             if array[x] == 'T':
-                print('Switch', x, 'is Low')
+                print('Switch', (x - 5), 'is Low')
             elif array[x] == 'F':
-                print('Switch ', x, 'is High')
+                print('Switch ', (x - 5), 'is High')
                 
 
-######################Read Switch Values######################
+######################Change Brightness values######################
+arduinoSerialData.write('SET_BRIGHTNESS:255\r'.encode())
+#arduinoSerialData.write('SET_COLOUR:9:0x000000\r'.encode())
 
-arduinoSerialData.write('SET_COLOUR:9:0xfff\r'.encode())
+#Loop through each section and colour blue for 1 sec
+for colour in range (0,8):
+    arduinoSerialData.write('SET_COLOUR:%d:0x00f\r'.encode() % colour)
+    time.sleep(1)
+    arduinoSerialData.write('SET_COLOUR:%d:0x000\r'.encode() % colour)
+
+
+print('\nTesting complete')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
