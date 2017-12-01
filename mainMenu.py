6703dbf441eb8bf95a3650f03b2b60e8a0b1d740
ciancostelloe <cian.costelloe@yahoo.ie>
2017-12-01 16:51:46 +0000

    Added GUI functions

--- mainMenu.py
+++ mainMenu.py
@@ -1,15 +1,32 @@
 ##Main Menu
 
+
 import sys
 import os
 from tkinter import *
 
-master = Tk()
 
-#######################Run Test#######################
+def printEG():
+    print("yes")
+
+
+#####################GUI Setup#######################    
+master = Tk()
+master.title("Young Scientist 2018")
+testing = Button(master, text = "Run Test", bg = "blue", fg = "white", command = runTest)
+testing.pack(fill = X)
+
+display = Button(master, text = "Display Mode", bg = "blue", fg = "white", command = printEG)
+display.pack(fill = X)
+
+game = Button(master, text = "Play Game", bg = "blue", fg = "white", command = printEG)
+game.pack(fill = X)
+
+
+#####################Run Test#######################
 def runTest():
     os.system("python initPODs.py")
     
 b = Button(master, text="Run Test", command=runTest)
-b.pack()
+b.pack(side=TOP)
 mainloop()
